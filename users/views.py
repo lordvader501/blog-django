fd868ff2e0a24e1f2b7933a20b5845cbcee15c17
lordvader501 <60027612+lordvader501@users.noreply.github.com>
2023-01-16 18:03:49 +0530

    created login page and user profile page

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
-def login(request):
-    pass
+    
 
 def register(request):
     if request.method == 'POST':
@@ -12,8 +12,18 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account {username} has been created. You can login!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
+
+class UserLogoutView(LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.success(request, "You have successfully logged out.")
+        return super().dispatch(request, *args, **kwargs)
