62f311ee0c582c06e59a6f207982e2174dc838e0
lordvader501 <60027612+lordvader501@users.noreply.github.com>
2023-01-17 14:41:55 +0530

    created profile update, new post create delte update, view post...

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 from .models import Post
 # Create your views here.
 
@@ -9,5 +11,45 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'contents']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, f'post created successfully')
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'contents']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, f'post updated successfully')
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm , UserUpdateForm, UpdateProfileForm
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -18,9 +18,27 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
 
+
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance= request.user)
+        p_form = UpdateProfileForm(request.POST, request.FILES, instance= request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been updated.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance= request.user)
+        p_form = UpdateProfileForm(instance= request.user.profile)
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
+    return render(request,'users/profile.html' , context)
+
+
 
 class UserLogoutView(LogoutView):
     def dispatch(self, request, *args, **kwargs):
