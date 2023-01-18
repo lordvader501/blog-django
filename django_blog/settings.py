bc87a2385c234e00497b03c11c1f5625bbaab0d0
lordvader501 <60027612+lordvader501@users.noreply.github.com>
2023-01-18 12:10:55 +0530

    added password reset option

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -135,3 +135,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGOUT_REDIRECT_URL = 'blog-home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
